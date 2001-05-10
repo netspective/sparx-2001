0f9856d969bf481c040d2e8fc03bb373d76e7629
snshah <snshah>
2001-05-10 06:00:30 +0000

    Remove @authors tag to prevent javadoc warnings during build.

--- Framework/java/com/xaf/task/TaskInitializeException.java
+++ Framework/java/com/xaf/task/TaskInitializeException.java
@@ -1,14 +1,5 @@
 package com.xaf.task;
 
-/**
- * Title:        The eXtensible Application Framework
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:      Netspective Communications Corporation
- * @author
- * @version 1.0
- */
-
 public class TaskInitializeException extends Exception
 {
     public TaskInitializeException(String msg)
--- Framework/java/com/xaf/value/ServletValueContext.java
+++ Framework/java/com/xaf/value/ServletValueContext.java
@@ -9,23 +9,48 @@ package com.xaf.value;
  * @version 1.0
  */
 
+import java.lang.reflect.*;
+import java.util.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class ServletValueContext implements ValueContext
 {
-	private HttpServletRequest request;
-	private HttpServletResponse response;
-	private ServletContext sc;
+	private CallbackManager callbacks;
+	private ServletContext servletContext;
+	private Servlet servlet;
+	private ServletRequest request;
+	private ServletResponse response;
 
-	public ServletValueContext(HttpServletRequest request, HttpServletResponse response, ServletContext sc)
+	public ServletValueContext(ServletContext context, Servlet servlet, ServletRequest request, ServletResponse response)
 	{
+		this.servletContext = context;
 		this.request = request;
 		this.response = response;
-		this.sc = sc;
+		this.servlet = servlet;
 	}
 
-	public final HttpServletRequest getRequest() { return request; }
-	public final HttpServletResponse getResponse() { return response; }
-	public final ServletContext getServletContext() { return sc; }
+	public final ServletRequest getRequest() { return request; }
+	public final ServletResponse getResponse() { return response; }
+	public final ServletContext getServletContext() { return servletContext; }
+	public final Servlet getServlet() { return servlet; }
+
+	public CallbackManager getCallbacks()
+	{
+		return callbacks;
+	}
+
+	public CallbackInfo getCallbackMethod(String callbackId)
+	{
+		if(callbacks == null)
+			return null;
+		return callbacks.getCallbackMethod(callbackId);
+	}
+
+	public void setCallbackMethod(String callbackId, Object owner, String methodName, Class[] paramTypes)
+	{
+		if(callbacks == null) callbacks = new CallbackManager();
+		callbacks.setCallbackMethod(callbackId, owner, methodName, paramTypes);
+	}
 }
