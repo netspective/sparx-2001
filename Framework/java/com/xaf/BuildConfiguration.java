64fc25f6b7a30de0335d159d6d2b6c662dd261ee
thua <thua>
2001-05-17 21:46:54 +0000

    Updated build value to 7

--- Framework/java/com/xaf/BuildConfiguration.java
+++ Framework/java/com/xaf/BuildConfiguration.java
@@ -8,7 +8,7 @@ public class BuildConfiguration
 	public static final int releaseNumber = 1;
 	public static final int versionMajor = 2;
 	public static final int versionMinor = 2;
-	public static final int buildNumber = 6;
+	public static final int buildNumber = 7;
 
 	static public final String getBuildPathPrefix()
 	{
