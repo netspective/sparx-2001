dc228447a71c47b9ef2fc1ef1c8a08cd60a032d2
snshah <snshah>
2001-06-02 21:53:08 +0000

    Sparx 1.2.5 Build 51 Release Notes ----------------------------------
    1. Added new style="confirm" attribute to <director> tag in <dialog> tag. This attribute, when set, automatically sets the submit caption to "Yes" and the cancel caption to "No".
    
    2. Added new attributes submit-url and cancel-url to <director> tag which, when set, will modify the default submit and cancel URLs. Setting cancel-url="back"  will allow programmers to simulate the "back" button instead of automatically going to the "caller" of the dialog. This is very useful with nested dialogs. This problem should take care of the issue that Joe Cooper reported with nested dialogs in LIS (where pressing "cancel" didn't seem to do the Right Thing).
    
    3. Fixed the getCancelCaption() and getSubmitCaption() methods in DialogDirector. There was a bug in which the caption and submit captions were being returned as the same value. Reported by Moorthy.
    
    4. Fixed problem in DialogContextListener where the isDialogValid() method was  not being called at the proper times (when inside a JSP). Reported by Moorthy.
    
    5. Added a new method called dialogNeedsValidation() in DialogContextListener for those cases where a dialog validation should be forced even if the fields in the dialog don't require validation.

--- Framework/java/com/xaf/BuildConfiguration.java
+++ Framework/java/com/xaf/BuildConfiguration.java
@@ -8,7 +8,7 @@ public class BuildConfiguration
 	public static final int releaseNumber = 1;
 	public static final int versionMajor = 2;
 	public static final int versionMinor = 5;
-	public static final int buildNumber = 45;
+	public static final int buildNumber = 51;
 
 	static public final String getBuildPathPrefix()
 	{
