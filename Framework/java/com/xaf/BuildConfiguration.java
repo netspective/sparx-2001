62ed49cfbef862470ce361c6bc6ea6782d98180b
thua <thua>
2001-09-10 21:21:07 +0000

    [Version 1.2.8 Build 23] - Updated client side script to handle different browsers. - Updated Text Field to accept new attribute value for "read-only" attribute. The new     value is called "browser" and this will display a readonly text field box.
        Example:
        <field.text name="orggroup" caption="Organizational Group "
        read-only="browser">
    - added new JS event "onclick" to handle radio buttons and checkboxes.
    - fixed auto-include tables of query definition dialogs so that they
      are included even when no fields reference them.
    - introduced new tag to support SQL group by expression in query definition dialogs.
      Example: <group-by field="order_id"/>

--- Framework/java/com/xaf/BuildConfiguration.java
+++ Framework/java/com/xaf/BuildConfiguration.java
@@ -8,7 +8,7 @@ public class BuildConfiguration
 	public static final int releaseNumber = 1;
 	public static final int versionMajor = 2;
 	public static final int versionMinor = 8;
-	public static final int buildNumber = 22;
+	public static final int buildNumber = 23;
 
 	static public final int getReleaseNumber() { return releaseNumber; }
 	static public final int getVersionMajor() { return versionMajor; }
