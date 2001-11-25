b5eec81e5372fe0e5e2ca045e61e8806f3ddb509
snshah <snshah>
2001-11-25 07:03:24 +0000

    [Version 1.2.8 Build 47] This is the build.bat that is required to build application-specific components for XAF applications. It simply sets up classpaths and directories and then calls Ant and runs Framework/tools/app-build.xml. This build.bat file should be created and maintained at the root level of each application.

---
+++ web-cura/build.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set FRAMEWORK_ROOT=c:\projects\Framework
+set APP_ROOT=c:\projects\web-cura
+set APP_CLASSES=%APP_ROOT%\Site\WEB-INF\classes
+
+set BUILD_FILE=%FRAMEWORK_ROOT%\tools\app-build.xml
+
+set JAVA_HOME=C:\utils\java\jdk1.3.1
+set ANT_HOME=C:\utils\java\jakarta-ant-1.3
+set XERCES_JAR=C:\utils\java\xerces-1_4_1\xerces.jar
+set XALAN_JAR=C:\utils\java\xalan-j_2_1_0\bin\xalan.jar
+set OROMATCHER_JAR=C:\utils\java\jakarta-oro-2.0.3\jakarta-oro-2.0.3.jar
+set LOG4J_JAR=C:\utils\java\jakarta-log4j-1.1.2\dist\lib\log4j.jar
+set SERVLETAPI_JAR=C:\utils\app-servers\resin-2.0.2\lib\jsdk23.jar
+set JDBC2X_JAR=C:\utils\app-servers\resin-2.0.2\lib\jdbc2_0-stdext.jar
+set FRAMEWORK_JAR=%FRAMEWORK_ROOT%\tools\idea\classes
+
+if exist "%JAVA_HOME%\lib\tools.jar" set JAVACP=%JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%\lib\classes.zip" set JAVACP=%CLASSPATH%;%JAVA_HOME%\lib\classes.zip
+
+set USE_CLASS_PATH=%APP_CLASSES%;%XERCES_JAR%;%FRAMEWORK_JAR%;%OROMATCHER_JAR%;%LOG4J_JAR%;%SERVLETAPI_JAR%;%JDBC2X_JAR%;%XALAN_JAR%;%JAVACP%
+
+java -Dant.home=%ANT_HOME% -classpath %USE_CLASS_PATH%;%ANT_HOME%\lib\ant.jar org.apache.tools.ant.Main -Dbasedir=%APP_ROOT% -buildfile %BUILD_FILE% %1 %2 %3 %4 %5
