310c345be4110f6dbdc3d4159b11ec7946b38da0
snshah <snshah>
2002-02-08 21:38:23 +0000

    [Version 2.0.2 Build 6] New "Pending Data" feature in dialogs now allows dialogs to accept data without complete client and server side validation of data. This allows data to be partially accepted, converted to XML, stored in a file or database, and then retrieved and later edited and completed.
    1) Pending Data is "turned on" using the new pending-caption="Pending Caption" in <director> tag of <dialog>. As long as a non-null pending-caption attribute is found in the director, a pending button is created. There is basic support for allowing other types of pending controls (checkbox, radio, etc) but they are not enabled yet. Let me know if they're needed.
    
       e.g. <director submit-caption="Submit" pending-caption=" Pending "/>
    
    2) DialogContext now has a new method called "isPending()" that is set to true when the "Pending Data" button is pressed. The idea is that at the beginning of the dialog's execute method the programmer will check this method to see if the data should be processed or temporarily saved.
    
       e.g. if(dc.isPending()) do something
    
    3) DialogContext now has 4 new methods for serializing and de-serializing the "pending data" to and from an XML stream. These methods can read/write into a string or a DOM object. They are called: String getAsXml(), and void setFromXml(String xml), void exportToXml(Element), and void importFromXml(Element). The easiest methods to use are getAsXml() and setFromXml() using the string methods (which ultimately use exportToXml() and importFromXml()).
    
    in a dialog subclass:
    
    public void populateData()
    {
       if(..data is pending...)
       {
         String xml = get data from database;
         dc.setFromXml(xml);
         return;
       }
    
       ... data is not pending ... do actual work
    }
    
    public void execute(...)
    {
       if(dc.isPending())
       {
         String xml = dc.getAsXml();
         ... store the xml somewhere ...
         return;
       }
    
       ... data is not pending ... do actual work
    }
    
    4) The DialogContext getDebugHtml() method has been improved to now show the XML that is generated by calling getAsXml() and displaying it as HTML.

--- Sparx/java/com/netspective/sparx/BuildConfiguration.java
+++ Sparx/java/com/netspective/sparx/BuildConfiguration.java
@@ -51,7 +51,7 @@
  */
 
 /**
- * $Id: BuildConfiguration.java,v 1.8 2002-02-06 20:26:19 snshah Exp $
+ * $Id: BuildConfiguration.java,v 1.9 2002-02-08 21:38:23 snshah Exp $
  */
 
 package com.netspective.sparx;
@@ -69,7 +69,7 @@ public class BuildConfiguration
     public static final int releaseNumber = 2;
     public static final int versionMajor = 0;
     public static final int versionMinor = 2;
-    public static final int buildNumber = 5;
+    public static final int buildNumber = 6;
 
     static public final int getReleaseNumber()
     {
