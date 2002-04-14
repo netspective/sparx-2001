3b8f0eef6aff23e6ee0750ff337e5692dbb7b3c3
snshah <snshah>
2002-04-14 14:13:56 +0000

    Modified the app-build.bat file so that it is more automated and added new "start-sparx-app" target that can be used to startup a new sparx app (including all required libraries, templates, configuration changes, etc). Now, the process to start a new project is:
    1) Create a new subdirectory somewhere. Lets assume it's called "web-start".
    2) In the web-start directory, copy SPARX_HOME/tools/app-build.bat as build.bat
    3) Run "build start-sparx-app". This will create all required subdirectories, copy files, make configuration changes, etc.
    4) Go to web-start/Site/WEB-INF and copy either resin.web.xml or tomcat.web.xml (or otherwise setup a web.xml file)
    5) Add the application to the application server's contexts
    6) Start using the app

--- Sparx/tools/app-build.bat
+++ Sparx/tools/app-build.bat
@@ -1,24 +1,64 @@
 @echo off
 
-set SPARX_HOME=c:\projects\Sparx
-set APP_ROOT=c:\projects\web-cura
+REM **************************************************************************
+REM ** Setup location of Sparx distribution, application build file, and    **
+REM ** the Sparx JAR file. Just set SPARX_HOME and the others will be set.  **
+REM **************************************************************************
+
+set SPARX_HOME=C:\Projects\Sparx
+set SPARX_JAR=%SPARX_HOME%\lib\sparx.jar
+set SPARX_REDIST_HOME=%SPARX_HOME%\lib\redist
+set APP_BUILD_FILE=%SPARX_HOME%\tools\app-build.xml
+
+
+REM **************************************************************************
+REM ** It's important to set the Application's root directory so that the   **
+REM ** build file can use APP_ROOT/Site/WEB-INF/classes to locate and       **
+REM ** compile the application's java classes and to do automatic setup of  **
+REM ** configuration files using the target 'setup-sparx'. If you are using **
+REM ** Windows 2000, XP, or NT you do not need to set APP_ROOT, it will be  **
+REM ** set to the current working directory automatically.                  **
+REM **                                                                      **
+REM ** NOTE: The command "for %%D in (.) do set APP_ROOT" will only work in **
+REM ** Windows NT/2k/XP. If you are not running under Windows NT/2k/XP then **
+REM ** you need to replace the line with with                               **
+REM **    set APP_ROOT=[your path name]                                     **
+REM **************************************************************************
+
+for %%D in (.) do set APP_ROOT=%%~fD
 set APP_CLASSES=%APP_ROOT%\Site\WEB-INF\classes
 
-set BUILD_FILE=%SPARX_HOME%\tools\app-build.xml
+
+REM **************************************************************************
+REM ** Setup location of all the Sparx prerequisites                        **
+REM **   Apache Xerces 1.4 or above (http://xml.apache.org)                 **
+REM **   Apache Xalan 2.1 or above (http://xml.apache.org)                  **
+REM **   Jakarta ORO Matcher 2.0 or above (http://jakarta.apache.org)       **
+REM **   Jakarta Log4J 1.1 or above (http://jakarta.apache.org)             **
+REM **   Java Servlet API 2.2 or above (http://java.sun.com)                **
+REM **   Java JDBC 2.0 Standard Extensions (http://java.sun.com)            **
+REM **************************************************************************
 
 set JAVA_HOME=C:\utils\java\jdk1.3.1
-set ANT_HOME=C:\utils\java\jakarta-ant-1.3
-set XERCES_JAR=C:\utils\java\xerces-1_4_1\xerces.jar
-set XALAN_JAR=C:\utils\java\xalan-j_2_1_0\bin\xalan.jar
-set OROMATCHER_JAR=C:\utils\java\jakarta-oro-2.0.3\jakarta-oro-2.0.3.jar
-set LOG4J_JAR=C:\utils\java\jakarta-log4j-1.1.2\dist\lib\log4j.jar
-set SERVLETAPI_JAR=C:\utils\app-servers\resin-2.0.4\lib\jsdk23.jar
-set JDBC2X_JAR=C:\utils\app-servers\resin-2.0.4\lib\jdbc2_0-stdext.jar
-set SPARX_JAR=%SPARX_HOME%\lib\sparx.jar
 
-if exist "%JAVA_HOME%\lib\tools.jar" set JAVACP=%JAVA_HOME%\lib\tools.jar
-if exist "%JAVA_HOME%\lib\classes.zip" set JAVACP=%CLASSPATH%;%JAVA_HOME%\lib\classes.zip
+set ANT_JAR=%SPARX_REDIST_HOME%\ant.jar
+set XERCES_JAR=%SPARX_REDIST_HOME%\xerces.jar
+set XALAN_JAR=%SPARX_REDIST_HOME%\xalan.jar
+set OROMATCHER_JAR=%SPARX_REDIST_HOME%\oro.jar
+set LOG4J_JAR=%SPARX_REDIST_HOME%\log4j.jar
+set BSF_JAR=%SPARX_REDIST_HOME%\bsf.jar
+set BSF_JS_JAR=%SPARX_REDIST_HOME%\js.jar
+set SERVLETAPI_JAR=%SPARX_REDIST_HOME%\servlet.jar
+set JDBC2X_JAR=%SPARX_REDIST_HOME%\jdbc.jar
 
-set USE_CLASS_PATH=%APP_CLASSES%;%XERCES_JAR%;%SPARX_JAR%;%OROMATCHER_JAR%;%LOG4J_JAR%;%SERVLETAPI_JAR%;%JDBC2X_JAR%;%XALAN_JAR%;%JAVACP%
+if exist "%JAVA_HOME%/lib/tools.jar" set JAVACP=%JAVA_HOME%/lib/tools.jar
+if exist "%JAVA_HOME%/lib/classes.zip" set JAVACP=%CLASSPATH%;%JAVA_HOME%/lib/classes.zip
 
-java -Dant.home=%ANT_HOME% -classpath %USE_CLASS_PATH%;%ANT_HOME%\lib\ant.jar org.apache.tools.ant.Main -Dbasedir=%APP_ROOT% -buildfile %BUILD_FILE% %1 %2 %3 %4 %5
+set USE_CLASS_PATH=%APP_CLASSES%;%XERCES_JAR%;%SPARX_JAR%;%OROMATCHER_JAR%;%LOG4J_JAR%;%SERVLETAPI_JAR%;%JDBC2X_JAR%;%XALAN_JAR%;%JAVACP%;%ANT_JAR%;%BSF_JAR%;%BSF_JS_JAR%
+
+
+REM **************************************************************************
+REM ** Now that all the variables are set, execute Ant                      **
+REM **************************************************************************
+
+java -classpath %USE_CLASS_PATH% org.apache.tools.ant.Main -Dbasedir=%APP_ROOT% -buildfile %APP_BUILD_FILE% %1 %2 %3 %4 %5
