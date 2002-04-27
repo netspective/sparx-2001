33d12a2b9ac46c7a088da94352e43a6a97fbac35
sjaveed <sjaveed>
2002-04-27 01:03:59 +0000

    Added: * Latest version with recreation of AbstractTable.insert() and executeDml()

--- web-library/Site/WEB-INF/classes/library/BookInfo.java
+++ web-library/Site/WEB-INF/classes/library/BookInfo.java
@@ -20,8 +20,9 @@ import dal.domain.row.BookInfoRow;
 import dal.DataAccessLayer;
 
 import dialog.context.library.BookInfoContext;
-
 import com.netspective.sparx.xaf.sql.DmlStatement;
+
+
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +35,9 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.io.Writer;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.util.List;
+
 
 public class BookInfo   extends Dialog
 {
@@ -157,20 +161,134 @@ public class BookInfo   extends Dialog
 			DmlStatement dml = bkInfoRow.createInsertDml(bkInfoTbl);
 			bkInfoTbl.validateDmlValues(dml);
 
+			writer.write (bkInfoRow.toString() + "<br>");
+			writer.write (dml.toString() + "<br>");
+
 			if (!bkInfoRow.beforeInsert(cc, dml)) {
 				status = false;
 				writer.write ("bkInfoRow.beforeInsert() returned false!<br>");
+			} else {
+				writer.write ("bkInfoRow.beforeInsert() returned true!<br>");
 			}
 
-	        boolean successful = bkInfoTbl.executeDml(cc, bkInfoRow, dml, null);
+
+
+
+
+
+
+
+
+
+
+
+			boolean result = false;
+			PreparedStatement stmt = null;
+			try
+			{
+				stmt = cc.getConnection().prepareStatement(dml.getSql());
+
+				int columnNum = 1;
+				boolean[] bindValues = dml.getBindValues();
+				List columnValues = dml.getColumnValues();
+				if(bindValues != null)
+				{
+									// Need to use columnValues.size() since the dml may have removed
+									// columns if autoinc columns are not included in the SQL
+					for(int c = 0; c < columnValues.size(); c++)
+					{
+						if(bindValues[c])
+						{
+							stmt.setObject(columnNum, columnValues.get(c));
+							columnNum++;
+						}
+					}
+				}
+
+/*
+				Object[] additionalBindParams = null;
+
+				if(additionalBindParams != null)
+				{
+					for(int i = 0; i < additionalBindParams.length; i++)
+					{
+						stmt.setObject(columnNum, additionalBindParams[i]);
+						columnNum++;
+					}
+				}
+*/
+				int numRowsUpdated = stmt.executeUpdate();
+				writer.write ("stmt.executeUpdate returned" + numRowsUpdated + "<br>");
+				if (numRowsUpdated > 0) result = true;
+				writer.write ("stmt.execute() returned " + (result ? "true!" : "false!") + "<br>");
+			}
+			catch(SQLException e)
+			{
+				StringBuffer bindParams = new StringBuffer();
+				int columnNum = 1;
+				boolean[] bindValues = dml.getBindValues();
+				List columnValues = dml.getColumnValues();
+				if(bindValues != null)
+				{
+					for(int c = 0; c < columnValues.size(); c++)
+					{
+						if(bindValues[c])
+						{
+							Object value = columnValues.get(c);
+							if(columnNum > 1)
+								bindParams.append(", ");
+							bindParams.append(columnNum);
+							bindParams.append(": ");
+							bindParams.append(value == null ? "NULL" : (value.toString() + " {" + value.getClass().getName() + "}"));
+							columnNum++;
+						}
+					}
+				}
+/*
+				if(additionalBindParams != null)
+				{
+					for(int i = 0; i < additionalBindParams.length; i++)
+					{
+						Object value = additionalBindParams[i];
+						if(columnNum > 1)
+							bindParams.append(", ");
+						bindParams.append(columnNum);
+						bindParams.append(": ");
+						bindParams.append(value == null ? "NULL" : (value.toString() + " {" + value.getClass().getName() + "}"));
+						columnNum++;
+					}
+				}
+*/
+				throw new SQLException(e.toString() + " [" + dml.getSql() + "\n(bind " + (bindParams.length() > 0 ? bindParams.toString() : "none") + ")\n" + bkInfoRow.toString() + "]");
+			}
+			finally
+			{
+				if(stmt != null) stmt.close();
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			boolean successful = result;
+//	        boolean successful = bkInfoTbl.executeDml(cc, bkInfoRow, dml, null);
+	        writer.write ("executeDml " + (successful ? "succeeded" : "failed") + "!<br>");
+
 	        bkInfoRow.afterInsert(cc);
 
        		cc.returnConnection();
         	status = successful;
-
-
-			writer.write (bkInfoRow.toString() + "<br>");
-			writer.write (dml.toString() + "<br>");
 
 //			status = bkInfoTbl.insert(cc, bkInfoRow);
 			cc.commitTransaction();
