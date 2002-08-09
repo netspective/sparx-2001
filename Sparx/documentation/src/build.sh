d8e2ccdd9181399739b8dac4daedc07343258701
shahid.shah <shahid.shah>
2002-08-09 19:18:35 +0000

    *** empty log message ***

--- Sparx/documentation/src/build.sh
+++ Sparx/documentation/src/build.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-SPARX_HOME=$HOME/Projects/Framework
-export SPARX_HOME
-
-APP_ROOT=`pwd`
-APP_CLASSES=$APP_ROOT/Site/WEB-INF/classes
 BUILD_FILE=build.xml
+SPARX_HOME=`pwd`/../../
+SPARX_REDIST_HOME=$SPARX_HOME/lib/redist
 
-JAVA_LIB=/home/shared/utils/java/lib
-APP_SERVER_LIB=/home/shared/utils/java/app-servers/jakarta-tomcat-4.0.1/common/lib
+SPARX_JAR=$SPARX_HOME/lib/sparx.jar
+ANT_JAR=$SPARX_REDIST_HOME/ant.jar
+XERCES_JAR=$SPARX_REDIST_HOME/xerces.jar
+XALAN_JAR=$SPARX_REDIST_HOME/xalan.jar
+OROMATCHER_JAR=$SPARX_REDIST_HOME/oro.jar
+LOG4J_JAR=$SPARX_REDIST_HOME/log4j.jar
+SERVLETAPI_JAR=$SPARX_REDIST_HOME/servlet.jar
+JDBC2X_JAR=$SPARX_REDIST_HOME/jdbc.jar
 
-ANT_HOME=$JAVA_LIB/jakarta-ant-1.4.1
-XERCES_JAR=$JAVA_LIB/xerces-1_4_4/xerces.jar
-XALAN_JAR=$JAVA_LIB/xalan-j_2_1_0/bin/xalan.jar
-#XALAN_JAR=$JAVA_LIB/xalan-j_2_2_D14/bin/xml-apis.jar:$JAVA_LIB/xalan-j_2_2_D14/bin/xalan.jar
-OROMATCHER_JAR=$JAVA_LIB/jakarta-oro-2.0.4/jakarta-oro-2.0.4.jar
-LOG4J_JAR=$JAVA_LIB/jakarta-log4j-1.1.3/dist/lib/log4j.jar
-SERVLETAPI_JAR=$APP_SERVER_LIB/servlet.jar
-JDBC2X_JAR=$APP_SERVER_LIB/jdbc2_0-stdext.jar
-FRAMEWORK_JAR=$SPARX_HOME/lib/xaf-1_2_8.jar
-NETCOMPONENTS_JAR=$JAVA_LIB/NetComponents-1.3.8/lib/NetComponents-1.3.8.jar
-SOURCESTYLER_JAR=$JAVA_LIB/ant-source-styler/styler.jar
-JAVACP=$JAVA_HOME/lib/tools.jar
+if [ -f $JAVA_HOME/lib/tools.jar ]; then
+	JAVACP=$JAVA_HOME/lib/tools.jar
+fi
 
-USE_CLASS_PATH=$APP_CLASSES:$XERCES_JAR:$FRAMEWORK_JAR:$OROMATCHER_JAR:$LOG4J_JAR:$SERVLETAPI_JAR:$JDBC2X_JAR:$XALAN_JAR:$NETCOMPONENTS_JAR:$SOURCESTYLER_JAR:$JAVACP
+if [ -f $JAVA_HOME/lib/classes.zip ]; then
+	JAVACP=$JAVA_HOME/lib/classes.zip
+fi
 
-java -Dant.home=$ANT_HOME -classpath $USE_CLASS_PATH:$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/ant-optional.jar org.apache.tools.ant.Main -Dbasedir=$APP_ROOT -buildfile $BUILD_FILE $1 $2 $3 $4 $5
+USE_CLASS_PATH=$XERCES_JAR:$SPARX_JAR:$OROMATCHER_JAR:$LOG4J_JAR:$SERVLETAPI_JAR:$JDBC2X_JAR:$XALAN_JAR:$JAVACP
+
+java -classpath $USE_CLASS_PATH:$ANT_JAR org.apache.tools.ant.Main -buildfile $BUILD_FILE $1 $2 $3 $4 $5
