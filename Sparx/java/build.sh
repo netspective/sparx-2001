5f8833de720bde397e6b15695a4718cbf8a42e7e
shahid.shah <shahid.shah>
2002-08-10 03:26:38 +0000

    *** empty log message ***

--- Sparx/java/build.sh
+++ Sparx/java/build.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 BUILD_FILE=build.xml
+SPARX_REDIST_HOME=`pwd`/../lib/redist
 
-JAVA_LIB=/usr/local/oracle/java-library/common
-APP_SERVER_LIB=/usr/local/oracle/resin/lib
+ANT_JAR=$SPARX_REDIST_HOME/ant.jar
+XERCES_JAR=$SPARX_REDIST_HOME/xerces.jar
+XALAN_JAR=$SPARX_REDIST_HOME/xalan.jar
+OROMATCHER_JAR=$SPARX_REDIST_HOME/oro.jar
+LOG4J_JAR=$SPARX_REDIST_HOME/log4j.jar
+SERVLETAPI_JAR=$SPARX_REDIST_HOME/servlet.jar
+JDBC2X_JAR=$SPARX_REDIST_HOME/jdbc.jar
 
-ANT_HOME=$JAVA_LIB/jakarta-ant-1.4.1
-XERCES_JAR=$JAVA_LIB/xerces-1_4_4/xerces.jar
-XALAN_JAR=$JAVA_LIB/xalan-j_2_1_0/bin/xalan.jar
-OROMATCHER_JAR=$JAVA_LIB/jakarta-oro-2.0.4/jakarta-oro-2.0.4.jar
-LOG4J_JAR=$JAVA_LIB/jakarta-log4j-1.1.3/dist/lib/log4j.jar
-SERVLETAPI_JAR=$APP_SERVER_LIB/jsdk23.jar
-JDBC2X_JAR=$APP_SERVER_LIB/jdbc2_0-stdext.jar
-JAVACP=$JAVA_HOME/lib/tools.jar
+if [ -f $JAVA_HOME/lib/tools.jar ]; then
+	JAVACP=$JAVA_HOME/lib/tools.jar
+fi
 
-USE_CLASS_PATH=$APP_CLASSES:$XERCES_JAR:$FRAMEWORK_JAR:$OROMATCHER_JAR:$LOG4J_JAR:$SERVLETAPI_JAR:$JDBC2X_JAR:$XALAN_JAR:$JAVACP
+if [ -f $JAVA_HOME/lib/classes.zip ]; then
+	JAVACP=$JAVA_HOME/lib/classes.zip
+fi
 
-java -Dant.home=$ANT_HOME -classpath $USE_CLASS_PATH:$ANT_HOME/lib/ant.jar org.apache.tools.ant.Main -Dbasedir=$APP_ROOT -buildfile $BUILD_FILE $1 $2 $3 $4 $5
+USE_CLASS_PATH=$XERCES_JAR:$SPARX_JAR:$OROMATCHER_JAR:$LOG4J_JAR:$SERVLETAPI_JAR:$JDBC2X_JAR:$XALAN_JAR:$JAVACP
+
+java -classpath $USE_CLASS_PATH:$ANT_JAR org.apache.tools.ant.Main -buildfile $BUILD_FILE $1 $2 $3 $4 $5
