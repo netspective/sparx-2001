68a3cc78fdb1333d0bac68cbcbaf9046d1588eda
shahid.shah <shahid.shah>
2002-09-04 16:34:45 +0000

    [Release 2.0.15] BuildConfiguration is no longer touched by each build so it doesn't need to be removed.. Now, the only two local build files are SPARX_HOME/java/build.number and SPARX_HOME/web-shared/resources/sparx.build.properties (which is no longer in the CVS repository since it's always re-generated for each build).

--- Sparx/pre-cvs-update.bat
+++ Sparx/pre-cvs-update.bat
@@ -6,7 +6,5 @@ REM ** Prevent build errors associated with local build files
 REM ************************************************************
 
 erase java\build.number
-erase java\com\netspective\sparx\BuildConfiguration.java
-erase web-shared\resources\sparx.build.properties
 
 echo Build files cleaned up, you may now do a CVS update.
