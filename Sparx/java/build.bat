0a1a9554169fc062951caffc821f3ec2242a04bb
shahid.shah <shahid.shah>
2002-09-05 20:50:38 +0000

    Migrated the majority of the class path specification into build.xml away from build.bat.

--- Sparx/java/build.bat
+++ Sparx/java/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM $Id: build.bat,v 1.6 2002-09-02 22:51:50 shahid.shah Exp $
+REM $Id: build.bat,v 1.7 2002-09-05 20:50:38 shahid.shah Exp $
 
 REM **************************************************************************
 REM ** This script should be be run from the SPARX_HOME\java directory.     **
@@ -8,55 +8,16 @@ REM ** It is basically a "launcher" for Ant and the actual work is done in  **
 REM ** the build.xml file.                                                  **
 REM **************************************************************************
 
-REM **************************************************************************
-REM ** NOTE: The command "for %%D in (.) do set BASEDIR" will only work in  **
-REM ** Windows NT/2k/XP. If you are not running under Windows NT/2k/XP then **
-REM ** you need to replace the line with with                               **
-REM **    set BASEDIR=[your path name]                                      **
-REM ** The SPARX_HOME, if not set, is assumed to be one level above this    **
-REM ** directory                                                            **
-REM **************************************************************************
+if "%JAVA_HOME%" == "" echo Error: JAVA_HOME environment variable is not set. && goto end
+if "%SPARX_HOME%" == "" set SPARX_HOME=..\
+if "%SPARX_REDIST_LIB%" == "" set SPARX_REDIST_LIB=%SPARX_HOME%\lib\redist
 
-for %%D in (.) do set BASEDIR=%%~fD
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java
+if not exist "%JAVACMD%.exe" echo Error: "%JAVACMD%.exe" not found - check JAVA_HOME && goto end
 
-if "%JAVA_HOME%" == "" set JAVA_HOME=C:\utils\java\jdk1.3.1
-if "%SPARX_HOME%" == "" set SPARX_HOME=%BASEDIR%\..
+if exist "%JAVA_HOME%/lib/tools.jar" set JAVACP=%JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%/lib/classes.zip" set JAVACP=%JAVACP%;%JAVA_HOME%\lib\classes.zip
 
-echo JAVA_HOME is %JAVA_HOME%
-echo SPARX_HOME is %SPARX_HOME%
+%JAVACMD% -classpath %SPARX_REDIST_LIB%\ant.jar;%SPARX_REDIST_LIB%\ant-optional.jar;%SPARX_REDIST_LIB%\xerces.jar;%JAVACP% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-REM **************************************************************************
-REM ** Setup location of all the Sparx prerequisites                        **
-REM **   Apache Xerces 1.4 or above (http://xml.apache.org)                 **
-REM **   Apache Xalan 2.1 or above (http://xml.apache.org)                  **
-REM **   Jakarta ORO Matcher 2.0 or above (http://jakarta.apache.org)       **
-REM **   Jakarta Log4J 1.1 or above (http://jakarta.apache.org)             **
-REM **   Java Servlet API 2.2 or above (http://java.sun.com)                **
-REM **   Java JDBC 2.0 Standard Extensions (http://java.sun.com)            **
-REM **************************************************************************
-
-set SPARX_REDIST_LIB=%SPARX_HOME%\lib\redist
-
-set ANT_JAR=%SPARX_REDIST_LIB%\ant.jar;%SPARX_REDIST_LIB%\ant-optional.jar
-set XMLAPIS_JAR=%SPARX_REDIST_LIB%\xml-apis.jar
-set XERCES_JAR=%SPARX_REDIST_LIB%\xerces.jar
-set XALAN_JAR=%SPARX_REDIST_LIB%\xalan.jar
-set OROMATCHER_JAR=%SPARX_REDIST_LIB%\oro.jar
-set LOG4J_JAR=%SPARX_REDIST_LIB%\log4j.jar
-set BSF_JAR=%SPARX_REDIST_LIB%\bsf.jar
-set BSF_JS_JAR=%SPARX_REDIST_LIB%\js.jar
-set JUNIT_JAR=%SPARX_REDIST_LIB%\junit.jar
-
-set SERVLETAPI_JAR=%SPARX_REDIST_LIB%\servlet.jar
-set JDBC2X_JAR=%SPARX_REDIST_LIB%\jdbc.jar
-
-if exist "%JAVA_HOME%/lib/tools.jar" set JAVACP=%JAVA_HOME%/lib/tools.jar
-if exist "%JAVA_HOME%/lib/classes.zip" set JAVACP=%CLASSPATH%;%JAVA_HOME%/lib/classes.zip
-
-set USE_CLASS_PATH=%XMLAPIS_JAR%;%XERCES_JAR%;%OROMATCHER_JAR%;%LOG4J_JAR%;%SERVLETAPI_JAR%;%JDBC2X_JAR%;%XALAN_JAR%;%JAVACP%;%ANT_JAR%;%BSF_JAR%;%BSF_JS_JAR%;%JUNIT_JAR%
-
-REM **************************************************************************
-REM ** Now that all the variables are set, execute Ant                      **
-REM **************************************************************************
-
-java -classpath %USE_CLASS_PATH% org.apache.tools.ant.Main -Dbasedir=%BASEDIR% -buildfile build.xml %1 %2 %3 %4 %5
+:end
