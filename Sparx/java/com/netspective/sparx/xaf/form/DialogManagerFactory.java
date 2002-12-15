c83a00ca10984cd2fa020526dfd8430db6d651d5
shahid.shah <shahid.shah>
2002-12-15 17:50:55 +0000

    [Version 2.1.25] Changed method calls for new FactoryEvent and DialogManager.getDialog() calls.

--- Sparx/java/com/netspective/sparx/xaf/form/DialogManagerFactory.java
+++ Sparx/java/com/netspective/sparx/xaf/form/DialogManagerFactory.java
@@ -3,55 +3,55 @@
  *
  * Netspective Corporation permits redistribution, modification and use
  * of this file in source and binary form ("The Software") under the
- * Netspective Source License ("NSL" or "The License"). The following 
- * conditions are provided as a summary of the NSL but the NSL remains the 
+ * Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the
  * canonical license and must be accepted before using The Software. Any use of
- * The Software indicates agreement with the NSL. 
+ * The Software indicates agreement with the NSL.
  *
  * 1. Each copy or derived work of The Software must preserve the copyright
  *    notice and this notice unmodified.
  *
- * 2. Redistribution of The Software is allowed in object code form only 
- *    (as Java .class files or a .jar file containing the .class files) and only 
- *    as part of an application that uses The Software as part of its primary 
- *    functionality. No distribution of the package is allowed as part of a software 
- *    development kit, other library, or development tool without written consent of 
- *    Netspective Corporation. Any modified form of The Software is bound by 
+ * 2. Redistribution of The Software is allowed in object code form only
+ *    (as Java .class files or a .jar file containing the .class files) and only
+ *    as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software
+ *    development kit, other library, or development tool without written consent of
+ *    Netspective Corporation. Any modified form of The Software is bound by
  *    these same restrictions.
- * 
- * 3. Redistributions of The Software in any form must include an unmodified copy of 
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of
  *    The License, normally in a plain ASCII text file unless otherwise agreed to,
  *    in writing, by Netspective Corporation.
  *
- * 4. The names "Sparx" and "Netspective" are trademarks of Netspective 
- *    Corporation and may not be used to endorse products derived from The 
- *    Software without without written consent of Netspective Corporation. "Sparx" 
- *    and "Netspective" may not appear in the names of products derived from The 
+ * 4. The names "Sparx" and "Netspective" are trademarks of Netspective
+ *    Corporation and may not be used to endorse products derived from The
+ *    Software without without written consent of Netspective Corporation. "Sparx"
+ *    and "Netspective" may not appear in the names of products derived from The
  *    Software without written consent of Netspective Corporation.
  *
- * 5. Please attribute functionality to Sparx where possible. We suggest using the 
+ * 5. Please attribute functionality to Sparx where possible. We suggest using the
  *    "powered by Sparx" button or creating a "powered by Sparx(tm)" link to
  *    http://www.netspective.com for each application using Sparx.
  *
- * The Software is provided "AS IS," without a warranty of any kind. 
+ * The Software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
  * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
  * OR NON-INFRINGEMENT, ARE HEREBY DISCLAIMED.
  *
  * NETSPECTIVE CORPORATION AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING 
+ * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING
  * THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE
  * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
  * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
  * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
  * INABILITY TO USE THE SOFTWARE, EVEN IF HE HAS BEEN ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGES.      
+ * OF SUCH DAMAGES.
  *
  * @author Shahid N. Shah
  */
- 
+
 /**
- * $Id: DialogManagerFactory.java,v 1.4 2002-10-16 03:14:57 shahid.shah Exp $
+ * $Id: DialogManagerFactory.java,v 1.5 2002-12-15 17:50:55 shahid.shah Exp $
  */
 
 package com.netspective.sparx.xaf.form;
@@ -103,12 +103,12 @@ public class DialogManagerFactory implements Factory
             listeners.add(listener);
     }
 
-    public static void contentsChanged()
+    public static void contentsChanged(DialogManager instance)
     {
         if(listeners == null)
             return;
 
-        FactoryEvent event = new FactoryEvent(DialogManagerFactory.class);
+        FactoryEvent event = new FactoryEvent(DialogManagerFactory.class, instance);
         for(Iterator i = listeners.iterator(); i.hasNext();)
         {
             FactoryListener listener = (FactoryListener) i.next();
@@ -137,7 +137,7 @@ public class DialogManagerFactory implements Factory
         {
             activeManager = new DialogManager(new File(file));
             managers.put(file, activeManager);
-            contentsChanged();
+            contentsChanged(activeManager);
         }
         return activeManager;
     }
@@ -300,7 +300,7 @@ public class DialogManagerFactory implements Factory
                 return;
             }
 
-            com.netspective.sparx.xaf.form.Dialog dialog = manager.getDialog(dialogName);
+            com.netspective.sparx.xaf.form.Dialog dialog = manager.getDialog(vc.getServletContext(), dialogName);
             if(dialog == null)
             {
                 out.write("Dialog '" + dialogName + "' not found in manager '" + manager + "'.");
