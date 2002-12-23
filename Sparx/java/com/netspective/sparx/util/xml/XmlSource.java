d7ef3ac25adfb1114ae9b0708aebc27ac07b6bfd
shahid.shah <shahid.shah>
2002-12-23 04:46:40 +0000

    Added new static convenience methods in XmlSource for processing DOM documents.

--- Sparx/java/com/netspective/sparx/util/xml/XmlSource.java
+++ Sparx/java/com/netspective/sparx/util/xml/XmlSource.java
@@ -3,55 +3,55 @@
  *
  * Netspective Corporation permits redistribution, modification and use
  * of this file in source and binary form ("The Software") under the
- * Netspective Source License ("NSL" or "The License"). The following 
- * conditions are provided as a summary of the NSL but the NSL remains the 
+ * Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the
  * canonical license and must be accepted before using The Software. Any use of
- * The Software indicates agreement with the NSL. 
+ * The Software indicates agreement with the NSL.
  *
  * 1. Each copy or derived work of The Software must preserve the copyright
  *    notice and this notice unmodified.
  *
- * 2. Redistribution of The Software is allowed in object code form only 
- *    (as Java .class files or a .jar file containing the .class files) and only 
- *    as part of an application that uses The Software as part of its primary 
- *    functionality. No distribution of the package is allowed as part of a software 
- *    development kit, other library, or development tool without written consent of 
- *    Netspective Corporation. Any modified form of The Software is bound by 
+ * 2. Redistribution of The Software is allowed in object code form only
+ *    (as Java .class files or a .jar file containing the .class files) and only
+ *    as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software
+ *    development kit, other library, or development tool without written consent of
+ *    Netspective Corporation. Any modified form of The Software is bound by
  *    these same restrictions.
- * 
- * 3. Redistributions of The Software in any form must include an unmodified copy of 
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of
  *    The License, normally in a plain ASCII text file unless otherwise agreed to,
  *    in writing, by Netspective Corporation.
  *
- * 4. The names "Sparx" and "Netspective" are trademarks of Netspective 
- *    Corporation and may not be used to endorse products derived from The 
- *    Software without without written consent of Netspective Corporation. "Sparx" 
- *    and "Netspective" may not appear in the names of products derived from The 
+ * 4. The names "Sparx" and "Netspective" are trademarks of Netspective
+ *    Corporation and may not be used to endorse products derived from The
+ *    Software without without written consent of Netspective Corporation. "Sparx"
+ *    and "Netspective" may not appear in the names of products derived from The
  *    Software without written consent of Netspective Corporation.
  *
- * 5. Please attribute functionality to Sparx where possible. We suggest using the 
+ * 5. Please attribute functionality to Sparx where possible. We suggest using the
  *    "powered by Sparx" button or creating a "powered by Sparx(tm)" link to
  *    http://www.netspective.com for each application using Sparx.
  *
- * The Software is provided "AS IS," without a warranty of any kind. 
+ * The Software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
  * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
  * OR NON-INFRINGEMENT, ARE HEREBY DISCLAIMED.
  *
  * NETSPECTIVE CORPORATION AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING 
+ * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING
  * THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE
  * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
  * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
  * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
  * INABILITY TO USE THE SOFTWARE, EVEN IF HE HAS BEEN ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGES.      
+ * OF SUCH DAMAGES.
  *
  * @author Shahid N. Shah
  */
- 
+
 /**
- * $Id: XmlSource.java,v 1.9 2002-12-15 17:42:40 shahid.shah Exp $
+ * $Id: XmlSource.java,v 1.10 2002-12-23 04:46:40 shahid.shah Exp $
  */
 
 package com.netspective.sparx.util.xml;
@@ -87,7 +87,6 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.apache.xpath.XPathAPI;
 import org.apache.oro.text.regex.*;
-import org.apache.oro.text.perl.Perl5Util;
 import org.w3c.dom.Document;
 import org.w3c.dom.DocumentFragment;
 import org.w3c.dom.Element;
@@ -318,6 +317,38 @@ public class XmlSource
     {
         String attrValue = parent.getAttribute(name);
         return attrValue.length() > 0 ? attrValue : getTagText(parent, name, defaultText);
+    }
+
+    /**
+     * Given a attribute or tag name, find the item or return the defaultValue
+     */
+    public static String getAttrValueOrDefault(Element parent, String name, String defaultValue)
+    {
+        String attrValue = parent.getAttribute(name);
+        return attrValue.length() > 0 ? attrValue : getTagText(parent, name, defaultValue);
+    }
+
+    /**
+     * Given an attribute name, check to see if it has a value -- if not, set it to default
+     */
+    public static void setAttrValueDefault(Element parent, String name, String defaultValue)
+    {
+        String attrValue = parent.getAttribute(name);
+        if(attrValue.length() == 0 && defaultValue != null && defaultValue.length() > 0)
+            parent.setAttribute(name, defaultValue);
+    }
+
+    public static Element getOrCreateElement(Element parent, String name)
+    {
+        NodeList nl = parent.getElementsByTagName(name);
+        if(nl.getLength() == 0)
+        {
+            Element newElem = parent.getOwnerDocument().createElement(name);
+            parent.appendChild(newElem);
+            return newElem;
+        }
+        else
+            return (Element) nl.item(0);
     }
 
     /**
