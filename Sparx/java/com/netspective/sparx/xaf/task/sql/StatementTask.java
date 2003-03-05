c8f67ec9f6b473affd122eb581783adf1237d4d3
aye.thu <aye.thu>
2003-03-05 23:37:48 +0000

    Added check to make sure the statement exists

--- Sparx/java/com/netspective/sparx/xaf/task/sql/StatementTask.java
+++ Sparx/java/com/netspective/sparx/xaf/task/sql/StatementTask.java
@@ -3,55 +3,55 @@
  *
  * Netspective Corporation permits redistribution, modification and use
  * of this file in source and binary form ("The Software") under the
- * Netspective Source License ("NSL" or "The License"). The following 
- * conditions are provided as a summary of the NSL but the NSL remains the 
+ * Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the
  * canonical license and must be accepted before using The Software. Any use of
- * The Software indicates agreement with the NSL. 
+ * The Software indicates agreement with the NSL.
  *
  * 1. Each copy or derived work of The Software must preserve the copyright
  *    notice and this notice unmodified.
  *
- * 2. Redistribution of The Software is allowed in object code form only 
- *    (as Java .class files or a .jar file containing the .class files) and only 
- *    as part of an application that uses The Software as part of its primary 
- *    functionality. No distribution of the package is allowed as part of a software 
- *    development kit, other library, or development tool without written consent of 
- *    Netspective Corporation. Any modified form of The Software is bound by 
+ * 2. Redistribution of The Software is allowed in object code form only
+ *    (as Java .class files or a .jar file containing the .class files) and only
+ *    as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software
+ *    development kit, other library, or development tool without written consent of
+ *    Netspective Corporation. Any modified form of The Software is bound by
  *    these same restrictions.
- * 
- * 3. Redistributions of The Software in any form must include an unmodified copy of 
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of
  *    The License, normally in a plain ASCII text file unless otherwise agreed to,
  *    in writing, by Netspective Corporation.
  *
- * 4. The names "Sparx" and "Netspective" are trademarks of Netspective 
- *    Corporation and may not be used to endorse products derived from The 
- *    Software without without written consent of Netspective Corporation. "Sparx" 
- *    and "Netspective" may not appear in the names of products derived from The 
+ * 4. The names "Sparx" and "Netspective" are trademarks of Netspective
+ *    Corporation and may not be used to endorse products derived from The
+ *    Software without without written consent of Netspective Corporation. "Sparx"
+ *    and "Netspective" may not appear in the names of products derived from The
  *    Software without written consent of Netspective Corporation.
  *
- * 5. Please attribute functionality to Sparx where possible. We suggest using the 
+ * 5. Please attribute functionality to Sparx where possible. We suggest using the
  *    "powered by Sparx" button or creating a "powered by Sparx(tm)" link to
  *    http://www.netspective.com for each application using Sparx.
  *
- * The Software is provided "AS IS," without a warranty of any kind. 
+ * The Software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
  * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
  * OR NON-INFRINGEMENT, ARE HEREBY DISCLAIMED.
  *
  * NETSPECTIVE CORPORATION AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING 
+ * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING
  * THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE
  * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
  * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
  * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
  * INABILITY TO USE THE SOFTWARE, EVEN IF HE HAS BEEN ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGES.      
+ * OF SUCH DAMAGES.
  *
  * @author Shahid N. Shah
  */
- 
+
 /**
- * $Id: StatementTask.java,v 1.13 2003-02-26 07:54:15 aye.thu Exp $
+ * $Id: StatementTask.java,v 1.14 2003-03-05 23:37:48 aye.thu Exp $
  */
 
 package com.netspective.sparx.xaf.task.sql;
@@ -66,6 +66,8 @@ import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Element;
 
@@ -423,15 +425,23 @@ public class StatementTask extends BasicTask
 
             if (produceReport && this.pageableReport)
             {
-                // Special Case: This static query must produce a report that is pageable
-                StatementDialog stmtDialog = new StatementDialog(stmtManager.getStatement(tc.getServletContext(), null, stmtName), getReport(), getSkin() != null ? getSkin().getValue(tc) : null, null);
-                stmtDialog.setRowsPerPage(getRowsPerPage());
-                DialogSkin skin = com.netspective.sparx.xaf.skin.SkinFactory.getInstance().getDialogSkin();
-                DialogContext dc = stmtDialog.createContext(context, tc.getServlet(),
-                        (javax.servlet.http.HttpServletRequest) tc.getRequest(),
-                        (javax.servlet.http.HttpServletResponse) tc.getResponse(), skin);
-                stmtDialog.prepareContext(dc);
-                stmtDialog.renderHtml(out, dc, false);
+                StatementInfo si = stmtManager.getStatement(tc.getServletContext(), null, stmtName);
+                if (si != null)
+                {
+                    StatementDialog stmtDialog = new StatementDialog(si, getReport(), getSkin() != null ? getSkin().getValue(tc) : null, null);
+                    stmtDialog.setRowsPerPage(rowsPerPage);
+                    if(stmtDialog != null)
+                    {
+                        DialogContext dc = stmtDialog.createContext(context, tc.getServlet(), (HttpServletRequest) tc.getRequest(),
+                                (HttpServletResponse) tc.getResponse(), SkinFactory.getInstance().getDialogSkin());
+                        stmtDialog.prepareContext(dc);
+                        stmtDialog.renderHtml(out, dc, true);
+                    }
+                }
+                else
+                {
+                    throw new TaskExecuteException("Statement '" + stmtName + "' does not exist");
+                }
             }
             else if(produceReport && storeValueSource == null)
             {
